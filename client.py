7bace970774ab23abc35c860bed91c9403c9c5b4
Angelin-Bonnet Olivia <oangelinbo@bs406-s31-08.insa-lyon.fr>
2015-11-26 11:02:44 +0100

    First version of client/server (not working)

---
+++ client.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*
+
+from Tkinter import*
+import time
+import os
+
+import serveur #SCCD9
+
+
+
+class Layout:
+
+	def __init__(self):
+
+		self.fenetre = Tk()
+		self.fenetre.title("Super Crazy Checkers Deluxe 9000 (v0.7)")
+		self.fenetre.geometry("900x600")
+
+
+		self.click = False
+
+		# game engine : rules, pace...
+		#self.game = SCCD9.Board()
+
+		self.plz_h = 600			# board height
+		self.plz_w = 600			# board width
+
+		self.length = 10
+
+		# graphical parameters
+		self.cs = 59 				# square size (height and width)
+		self.x_gap = 6 				# x gap between 2 squares
+		self.y_gap = 6 				# y gap between 2 squares
+		self.size = 54 				# piece size
+
+		# parameters for the cemetery (place where we draw the pieces that have been taken)
+		self.cemetery_cs = 18
+		self.cemetery_size = 12
+		self.cemetery_y_gap = 22
+
+
+
+		# Tkinter (graphical) objects
+		world = PanedWindow(self.fenetre,height=599,width=899)
+		self.playzone = Canvas(self.fenetre,height=599,width=599,bg='white')
+		controls = PanedWindow(self.fenetre,height=599,width=300,orient=VERTICAL)
+
+		self.playzone.create_rectangle(1,1,598,598)
+
+
+		# label indicating the player whose turn it is
+		self.player_now = StringVar()
+		self.player_now.set("Now playing : Green")
+		label_player = Label(controls,textvariable=self.player_now,height=4)
+
+		controls.add(label_player)
+
+
+		# canvas to draw the pieces taken by opponent
+		self.cemetery = Canvas(controls,height=100)
+
+		controls.add(self.cemetery)
+
+
+		# auto-rotation checkbox
+		self.autorot = IntVar()
+		self.autorot.set(0)
+		check_autorot = Checkbutton(controls,text="Auto-rotate",variable=self.autorot,height=15)
+
+		controls.add(check_autorot)
+
+
+		# reset button (hopefully will after be included in a menu)
+		reset_button = Button(controls,text="Restart",command=self.reset)
+
+		controls.add(reset_button)
+
+
+		world.add(self.playzone)
+		world.add(controls)
+		world.pack()
+
+
+		# event listener for left clicks on board
+		self.playzone.bind("<Button-1>", self.left_click)
+
+
+		# drawing the board for the first time
+		self.draw_grid_2(self.game.grid)
+
+
+
+	# drawing the grid with player 1 below
+	def draw_grid_1 (self, grid, queens = []) :
+
+		self.playzone.delete("all")
+		for i in xrange (len(grid)) :
+			for j in xrange (len(grid)) :
+				if grid[i][j] == -1 :
+					self.playzone.create_oval(self.plz_w-(self.cs*i+self.x_gap),self.plz_h-(self.cs*j+self.y_gap),self.plz_w-(self.cs*i+self.x_gap+self.size),self.plz_h-(self.cs*j+self.y_gap+self.size),outline='white')
+				else :
+					self.playzone.create_oval(self.plz_w-(self.cs*i+self.x_gap),self.plz_h-(self.cs*j+self.y_gap),self.plz_w-(self.cs*i+self.x_gap+self.size),self.plz_h-(self.cs*j+self.y_gap+self.size),outline='black')
+				
+				if grid[i][j] == 1 :
+					self.playzone.create_oval(self.plz_w-(self.cs*i+self.x_gap+self.size/4),self.plz_h-(self.cs*j+self.y_gap+self.size/4),self.plz_w-(self.cs*i+self.x_gap+3*self.size/4),self.plz_h-(self.cs*j+self.y_gap+3*self.size/4),outline='#d00',fill='#d00')
+					if [i,j] in queens :
+						self.playzone.create_oval(self.plz_w-(self.cs*i+self.x_gap+self.size/16),self.plz_h-(self.cs*j+self.y_gap+self.size/16),self.plz_w-(self.cs*i+self.x_gap+15*self.size/16),self.plz_h-(self.cs*j+self.y_gap+15*self.size/16),outline='#d00',fill='#d00')
+
+				if grid[i][j] == 2 :
+					self.playzone.create_oval(self.plz_w-(self.cs*i+self.x_gap+self.size/4),self.plz_h-(self.cs*j+self.y_gap+self.size/4),self.plz_w-(self.cs*i+self.x_gap+3*self.size/4),self.plz_h-(self.cs*j+self.y_gap+3*self.size/4),outline='#080',fill='#080')
+					if [i,j] in queens :
+						self.playzone.create_oval(self.plz_w-(self.cs*i+self.x_gap+self.size/16),self.plz_h-(self.cs*j+self.y_gap+self.size/16),self.plz_w-(self.cs*i+self.x_gap+15*self.size/16),self.plz_h-(self.cs*j+self.y_gap+15*self.size/16),outline='#080',fill='#080')
+
+
+
+	# drawing the grid with player 2 below
+	def draw_grid_2 (self, grid, queens = []) :
+
+		self.playzone.delete("all")
+		for i in xrange(len(grid)) :
+			for j in xrange(len(grid)) :
+				if grid[i][j] == -1 :
+					self.playzone.create_oval(self.cs*i+self.x_gap,self.cs*j+self.y_gap,self.cs*i+self.x_gap+self.size,self.cs*j+self.y_gap+self.size,outline='white')
+				else :
+					self.playzone.create_oval(self.cs*i+self.x_gap,self.cs*j+self.y_gap,self.cs*i+self.x_gap+self.size,self.cs*j+self.y_gap+self.size,outline='black')
+				
+				if grid[i][j] == 1 :
+					self.playzone.create_oval(self.cs*i+self.x_gap+self.size/4,self.cs*j+self.y_gap+self.size/4,self.cs*i+self.x_gap+3*self.size/4,self.cs*j+self.y_gap+3*self.size/4,outline='#d00',fill='#d00')
+					if [i,j] in queens :
+						self.playzone.create_oval(self.cs*i+self.x_gap+self.size/16,self.cs*j+self.y_gap+self.size/16,self.cs*i+self.x_gap+15*self.size/16,self.cs*j+self.y_gap+15*self.size/16,outline='#d00',fill='#d00')
+
+				if grid[i][j] == 2 :
+					self.playzone.create_oval(self.cs*i+self.x_gap+self.size/4,self.cs*j+self.y_gap+self.size/4,self.cs*i+self.x_gap+3*self.size/4,self.cs*j+self.y_gap+3*self.size/4,outline='#080',fill='#080')
+					if [i,j] in queens :
+						self.playzone.create_oval(self.cs*i+self.x_gap+self.size/16,self.cs*j+self.y_gap+self.size/16,self.cs*i+self.x_gap+15*self.size/16,self.cs*j+self.y_gap+15*self.size/16,outline='#080',fill='#080')
+
+
+
+	# drawing the cemetery (containing all pieces taken by the opponent)
+	def draw_cemetery (self, grid, queens = []) :
+
+		self.cemetery.delete("all")
+		# number of pieces taken = 20 - pieces left - queens (as a queen worths 2 pieces)
+		red_taken = 20 - sum([i.count(1) for i in grid]) - len([1 for i in queens if grid[i[0]][i[1]] == 1])
+		green_taken = 20 - sum([i.count(2) for i in grid]) - len([1 for i in queens if grid[i[0]][i[1]] == 2])
+		
+		# drawing on 2 lines, for easier understanding
+		for i in xrange(red_taken) :
+			if i < 10 :
+				self.cemetery.create_oval(self.cemetery_cs*i+self.x_gap,self.y_gap,self.cemetery_cs*i+self.x_gap+self.cemetery_size,self.y_gap+self.cemetery_size,outline='#d00',fill='#d00')
+			else :
+				self.cemetery.create_oval(self.cemetery_cs*(i-10)+self.x_gap,self.cemetery_y_gap+self.y_gap,self.cemetery_cs*(i-10)+self.x_gap+self.cemetery_size,self.y_gap+self.cemetery_y_gap+self.cemetery_size,outline='#d00',fill='#d00')
+
+		for i in xrange(green_taken) :
+			if i < 10 :
+				self.cemetery.create_oval(self.cemetery_cs*i+self.x_gap,self.y_gap+2*self.cemetery_y_gap,self.cemetery_cs*i+self.x_gap+self.cemetery_size,self.y_gap+2*self.cemetery_y_gap+self.cemetery_size,outline='#080',fill='#080')
+			else :
+				self.cemetery.create_oval(self.cemetery_cs*(i-10)+self.x_gap,self.y_gap+3*self.cemetery_y_gap,self.cemetery_cs*(i-10)+self.x_gap+self.cemetery_size,self.y_gap+3*self.cemetery_y_gap+self.cemetery_size,outline='#080',fill='#080')
+
+
+
+
+	# method called by a left click on board
+	def left_click(self, event) :
+
+		self.click = True
+
+		# getting where the click has happened
+		if self.autorot.get() == 1 :
+			if self.game.player == 2 :
+				x = event.x / (self.plz_h/self.length)
+				y = event.y / (self.plz_w/self.length)
+
+			else :
+				x = 9 - event.x/(self.plz_h/self.length)
+				y = 9 - event.y/(self.plz_w/self.length)
+		else :
+			x = event.x / (self.plz_h/self.length)
+			y = event.y / (self.plz_w/self.length)
+
+
+		#send identifiant_joueur, x, y 
+		#recv draw_grid_1, grid, queens OU 	draw_grid_2, grid, queens
+		#recv draw_cemetery, grid, queens	
+		#recv highlight_piece_1, highlight 	OU NON
+		#recv loop 							OU NON			
+
+		# # updating the drawing of the grid
+		# if self.game.player == 1 :
+		# 	self.draw_grid_1(self.game.grid,self.game.queens)
+		# else :
+		# 	self.draw_grid_2(self.game.grid,self.game.queens)
+
+		# self.draw_cemetery(self.game.grid,self.game.queens)
+
+
+		# # and highlighting the currently selected piece
+		# if self.game.highlight != [] :
+
+		# 	if self.game.player == 1 :
+		# 		self.highlight_piece_1(self.game.highlight)
+		# 	else :
+		# 		self.highlight_piece_2(self.game.highlight)
+
+
+		# if self.game.player == 1 :
+		# 	self.player_now.set("Now playing : Red")
+		# else :
+		# 	self.player_now.set("Now playing : Green")
+
+		# self.check_end()
+
+
+	def highlight_piece_1 (self, coords) :
+
+		self.playzone.create_oval(self.plz_w-(self.cs*coords[0]+self.x_gap+self.size/6),self.plz_h-(self.cs*coords[1]+self.y_gap+self.size/6),self.plz_w-(self.cs*coords[0]+self.x_gap+5*self.size/6),self.plz_h-(self.cs*coords[1]+self.y_gap+5*self.size/6),outline='black')
+
+
+
+	def highlight_piece_2 (self, coords) :
+
+		self.playzone.create_oval(self.cs*coords[0]+self.x_gap+self.size/6,self.cs*coords[1]+self.y_gap+self.size/6,self.cs*coords[0]+self.x_gap+5*self.size/6,self.cs*coords[1]+self.y_gap+5*self.size/6,outline='black')
+
+
+
+	# def check_end (self) :
+
+	# 	i = 0
+	# 	end = True
+	# 	while i < self.length and end == True :
+	# 		if 1 in self.game.grid[i] :
+	# 			end = False
+	# 		i = i+1
+
+	# 	if end == True :
+	# 		self.player_now.set("The green guy has won !")
+
+	# 	i = 0
+	# 	end = True
+	# 	while i < self.length and end == True :
+	# 		if 2 in self.game.grid[i] :
+	# 			end = False
+	# 		i = i+1
+
+	# 	if end == True :
+	# 		self.player_now.set("The red guy has won !")
+
+
+	def check_end(winner):
+		
+		# if winner == 'nowinner':
+		# 	break
+		
+		if winner =='player1':
+			self.player_now.set("The red guy has won !")
+
+		if winner =='player2':
+			self.player_now.set("The green guy has won !")
+
+
+
+
+
+
+
+
+	def reset (self) :
+
+		self.fenetre.destroy()
+		os.system("python SCCD9_interface.py")
+
+
+
+	# (Tkinter's mainloop)
+	def loop (self) :
+		#afficher à ton tour de jouer
+		#self.fenetre.mainloop()
+		self.click = False
+		while not self.click:
+			self.fenetre.update_idletasks()
+			self.fenetre.update()
+
+
+
+
+def main_client():
+	Checkers = Layout()
+	Checkers.loop()
+
+
+
---
+++ serveur.py
@@ -0,0 +1,637 @@
+# -*- coding: utf-8 -*
+
+from Tkinter import*
+import time
+import os
+
+
+class Board :
+
+	def __init__ (self, ip1, ip2) :
+
+		self.height = 10
+		self.width = 10
+		self.grid = [[-1 for i in xrange(0,self.height,1)] for j in xrange(0,self.width,1)]
+
+		self.init_board() 				# putting all pieces at their starting places
+
+
+		# variables holding the state of the game
+		self.ips = {"P1":ip1,"P2":ip2}
+		self.player = 2 				# whose turn it is to play
+		self.highlight = []				# currently selected piece (coordinates on the board)
+		self.moves = [] 				# possible moves for currently selected piece
+		self.takeovers = []				# possible moves (for currently selected piece) involving the takeover of another piece
+		self.play_again = False 		# does the player plays again ? (after a takeover)
+		self.player_end_turn = False
+
+		self.queens = [] 				# contains coordinates of all queens on the board (as they follow special rules)
+
+
+
+		# to check possible huffs
+		self.takeover_done = False
+		self.last_moved = []
+		self.huff_done = False
+		self.possible_huff = []
+		self.end = False
+
+
+
+	def __repr__ (self) :
+
+		string = ["----------------------------------------------------------------------------------------------------------------------------------------------------------"]
+		string.append("\n")
+		for i in xrange(0,len(self.grid),1) :
+			string.append("\t".join([str(j) for j in self.grid[i]]))
+			string.append("\n")
+		string.append("----------------------------------------------------------------------------------------------------------------------------------------------------------")
+		return "".join(string)
+
+
+	# putting all pieces at their starting places
+	def init_board (self) :
+
+		for i in xrange (0,self.height,2) :
+			for j in [0,2,6,8] :
+				self.grid[i][j] = 1 + j/5
+			self.grid[i][4] = 0
+
+		for i in xrange (1,self.height,2) :
+			for j in [1,3,7,9] :
+				self.grid[i][j] = 1 + j/5
+			self.grid[i][5] = 0
+
+		#print self
+
+
+
+
+	def left_click(x, y, player):
+		# there are 2 possibilities : whether the player wants to select a piece or to move it
+
+		if self.highlight == [] :					# if no piece is already selected (selecting a piece to move or huffing an opponent's one)
+			self.select(x,y) 						# we call the "select" method in the game engine
+														
+		else :											# else the player wants to move a piece
+			self.move(x,y)							# we call the "move" method
+
+		# updating the drawing of the grid
+		#send draw_grid_1, grid, queens 	JOUEUR 1
+		#send draw_grid_2, grid, queens		JOUEUR 2
+		#send draw_cemetery, grid, queens	2 JOUEURS
+		if not self.player_end_turn :
+			#send highlight_piece_1, highlight 	JOUEUR COURANT (self.player)
+			#send loop 							JOUEUR COURANT (self.player)
+		else :
+			return 0
+		# if player == 1 :
+		# 	self.draw_grid_1(self.game.grid,self.game.queens) 
+
+		# else :
+		# 	self.draw_grid_2(self.game.grid,self.game.queens) #SEND tell the client to draw the game
+
+		# self.draw_cemetery(self.game.grid,self.game.queens) #SEND tell the client to draw the game
+
+
+		# and highlighting the currently selected piece
+		# if self.game.highlight != [] :
+
+		# 	if self.game.player == 1 :
+		# 		self.highlight_piece_1(self.game.highlight)
+		# 	else :
+		# 		self.highlight_piece_2(self.game.highlight)
+
+
+		if player == 1 :
+			self.player_now.set("Now playing : Red")
+		else :
+			self.player_now.set("Now playing : Green")
+
+		self.check_end()
+
+
+
+
+	# if no piece is currently selected, we assume the player wants to pick one :
+	def select (self, x, y) :
+
+		if self.grid[x][y] > 0 :
+
+			self.possibilities(x,y)
+	
+			#print self.moves, self.takeovers 
+			# if an action (move or takeover) is possible with the current selected piece :
+			if self.moves != [] or self.takeovers != [] or [x,y] == self.possible_huff :
+
+				# if the player has selected an opponent's piece (to huff it), and if no huff has already been done by this player
+				if self.grid[x][y] != self.player and self.huff_done == False :
+
+					self.check_huff(x,y)
+
+				else :
+
+					self.highlight = [x,y]
+ 
+
+
+
+	# now the player wants to move the selected piece to the clicked square
+	def move (self, x, y) :
+
+
+		# if it is a standard move, for the first action of the player
+		if [x,y] in self.moves and self.play_again == False :
+
+			self.possible_huff = []
+
+			self.takeover_done = False
+
+			self.move_on_grid(x,y)
+
+			if self.takeovers != [] :    # if this piece could have overtake an opponent's one, it is eligible to be huffed
+				self.possible_huff = [x,y]
+				#print self.possible_huff
+
+			# end of player's turn
+			self.end_turn()
+
+
+		# if it is a takeover
+		elif [x,y] in self.takeovers :
+
+			self.possible_huff = []
+
+			self.takeover_done = True
+
+			self.move_on_grid(x,y)
+
+			# we need to delete the piece that has been taken, that's easy to do if a piece has done the takeover
+			if [x,y] not in self.queens :
+
+				temp = (self.highlight[0] - x)/2
+				temp2 = (self.highlight[1] - y)/2
+				self.grid[x+temp][y+temp2] = 0
+				if [x+temp,y+temp2] in self.queens :
+					self.queens.remove([x+temp,y+temp2])
+					#print self.queens
+
+			# but trickier if a queen has been played (we don't know where the piece was)
+			else :
+				self.queen_takeover(x,y)
+
+			self.takeovers = []
+
+			self.select(x,y)						# can this player play again ? (by taking another opponent's piece)				
+
+			if self.takeovers == [] :
+
+				self.end_turn()
+
+			else :
+
+				self.play_again = True		
+				# if yes, we keep the currently selected piece highlighted (so the player cannot cowardly play another one), and memorize the next possible takeover(s)
+				self.moves = []
+
+
+		# if the player has already taken one of his opponent's pieces and don't want to take another
+		elif self.play_again == True :
+
+			self.end_turn()
+			
+		# finally, the player may just want to unselect his piece, or want to try something impossible
+		else :
+
+			self.highlight = []
+		
+
+
+	# moving the selected piece on the board
+	def move_on_grid (self, x, y) :
+
+		self.grid[x][y] = self.grid[self.highlight[0]][self.highlight[1]]   # moving the piece
+		self.grid[self.highlight[0]][self.highlight[1]] = 0					# and leaving a blank where it comes from
+
+		self.last_moved = [x,y]
+
+		if [self.highlight[0],self.highlight[1]] in self.queens :			# if a queen has moved
+			self.queens.remove([self.highlight[0],self.highlight[1]])		# we need to update the queen positions
+			self.queens.append([x,y])
+			#print "A queen has been moved !"
+		
+		else :
+			self.check_queen(x,y)   										# else, we may check if this piece has become a queen
+
+
+
+	# special method called if a queen has made a takeover
+	def queen_takeover (self,x,y) :
+
+		done = False
+		i = self.highlight[0]
+		j = self.highlight[1]
+
+		while (i!=x or j!=y) and done == False :
+
+			if self.grid[i][j] > 0 :
+				self.grid[i][j] = 0
+				done = True
+				if [i,j] in self.queens :
+					self.queens.remove([i,j])
+
+			i = i + (x-i)/abs(x-i)
+			j = j + (y-j)/abs(y-j)
+
+
+
+	# checking a possible huff
+	def check_huff (self, x, y) :
+
+		#print self.moves, self.takeovers, self.takeover_done, x, self.last_moved[0], y, self.last_moved[1]
+
+		# conditions for a huff : 
+		# - the selected piece can do a takeover
+		# - the previous player hasn't made a takeover during his last turn
+		# - the selected piece isn't the last that has been moved (meaning the player hasn't yet been able to do the takeover)
+		# OR
+		# - the last piece that has been moved could have done a takeover instead
+		if (self.takeovers != [] and self.takeover_done == False and (x != self.last_moved[0] or y != self.last_moved[1]) and self.grid[x][y] != 0) or ([x,y] == self.possible_huff) :
+
+			if [x,y] in self.queens :
+				self.queens.remove([x,y])
+				#print self.queens
+			self.grid[x][y] = 0
+			self.huff_done = True
+			self.possible_huff = []
+
+
+
+	# checking if a new queen has appeared
+	def check_queen (self,x,y) :
+
+		if self.player == 1 :
+			if y == self.height-1 and [x,y] not in self.queens :
+				self.queens.append([x,y])
+				#print "New queen in town !"
+
+		if self.player == 2 :
+			if y == 0 and [x,y] not in self.queens :
+				self.queens.append([x,y])
+				#print "New queen in town !"
+			
+
+
+	# ending the player's turn (i.e. switching players and cleaning all temporary variables)
+	def end_turn (self) :
+
+		# if self.player == 1 :
+		# 	self.player = 2
+
+		# else :
+		# 	self.player = 1
+		self.player_end_turn = True
+
+		self.moves = []
+		self.highlight = []
+		self.takeovers = []
+		self.huff_done = False
+		self.play_again = False
+
+		#print("Turn has ended.")
+
+
+
+
+
+	def possibilities (self, x, y) :
+
+
+		self.moves = []
+		self.takeovers = []
+
+
+		# if a standard piece is selected
+		if [x,y] not in self.queens :
+
+
+
+			if self.grid[x][y] == 1 :
+
+
+				if x-1 > -1 and y-1 > -1 :
+
+					if self.grid[x-1][y-1] == 2 and x-2 > -1 and y-2 > -1 :
+
+						if self.grid[x-2][y-2] == 0 :
+
+							self.takeovers.append([x-2,y-2])
+							#print "Ce pion peut prendre celui à l'arrière gauche."
+
+
+				if x+1 < 10 and y-1 > -1 :
+
+					if self.grid[x+1][y-1] == 2 and x+2 < 10 and y-2 > -1 :
+
+						if self.grid[x+2][y-2] == 0 :
+
+							self.takeovers.append([x+2,y-2])
+							#print "Ce pion peut prendre celui à l'arrière droit."
+
+
+				if x-1 > -1 and y+1 < 10 :
+
+					if self.grid[x-1][y+1] == 0 and self.player == 1 :
+
+						self.moves.append([x-1,y+1])
+						#print "Ce pion peut être déplacé à l'avant gauche."
+
+					elif self.grid[x-1][y+1] == 2 and x-2 > -1 and y+2 < 10 :
+
+						if self.grid[x-2][y+2] == 0 :
+
+							self.takeovers.append([x-2,y+2])
+							#print "Ce pion peut prendre celui à l'avant gauche."
+
+
+
+				if x+1 < 10 and y+1 < 10 :
+
+					if self.grid[x+1][y+1] == 0 and self.player == 1 :
+
+						self.moves.append([x+1,y+1])
+						#print "Ce pion peut être déplacé à l'avant droit."
+
+					elif self.grid[x+1][y+1] == 2 and x+2 < 10 and y+2 < 10 :
+
+						if self.grid[x+2][y+2] == 0 :
+
+							self.takeovers.append([x+2,y+2])
+							#print "Ce pion peut prendre celui à l'avant droit."
+
+
+
+			if self.grid[x][y] == 2 :
+
+
+				if x-1 > -1 and y-1 > -1 :
+
+					if self.grid[x-1][y-1] == 0 and self.player == 2 :
+
+						self.moves.append([x-1,y-1])
+						#print "Ce pion peut être déplacé à l'avant gauche."
+
+					elif self.grid[x-1][y-1] == 1 and x-2 > -1 and y-2 > -1 :
+
+						if self.grid[x-2][y-2] == 0 :
+
+							self.takeovers.append([x-2,y-2])
+							#print "Ce pion peut prendre celui à l'avant gauche."
+
+
+				if x+1 < 10 and y-1 > -1 :
+
+					if self.grid[x+1][y-1] == 0 and self.player == 2 :
+
+						self.moves.append([x+1,y-1])
+						#print "Ce pion peut être déplacé à l'avant droit."
+
+					elif self.grid[x+1][y-1] == 1 and x+2 < 10 and y-2 > -1 :
+
+						if self.grid[x+2][y-2] == 0 :
+
+							self.takeovers.append([x+2,y-2])
+							#print "Ce pion peut prendre celui à l'avant droit."
+
+
+				if x-1 > -1 and y+1 < 10 :
+
+					if self.grid[x-1][y+1] == 1 and x-2 > -1 and y+2 < 10 :
+
+						if self.grid[x-2][y+2] == 0 :
+
+							self.takeovers.append([x-2,y+2])
+							#print "Ce pion peut prendre celui à l'arrière gauche."
+
+
+				if x+1 < 10 and y+1 < 10 :
+
+					if self.grid[x+1][y+1] == 1 and x+2 < 10 and y+2 < 10 :
+
+						if self.grid[x+2][y+2] == 0 :
+
+							self.takeovers.append([x+2,y+2])
+							#print "Ce pion peut prendre celui à l'arrière droit."
+
+
+
+		# if a queen is selected
+		else :
+
+			obstacle = False
+			takeover = False
+			i = x-1
+			j = y-1
+
+			while i>-1 and j>-1 and obstacle == False :
+
+				if self.grid[i][j] == self.grid[x][y] :
+					obstacle = True
+
+
+				elif self.grid[i][j] > 0 :
+
+					if i-1>-1 and j-1>-1 and takeover == False :
+						if self.grid[i-1][j-1] == 0 :
+							self.takeovers.append([i-1,j-1])
+							takeover = True
+							i = i-1
+							j = j-1
+						else :
+							obstacle = True
+					else :
+						obstacle = True
+
+
+				elif self.grid[i][j] == 0 :
+
+					if takeover == False :
+						self.moves.append([i,j])
+					else :
+						self.takeovers.append([i,j])
+
+				i = i-1
+				j = j-1
+
+
+
+			obstacle = False
+			takeover = False
+			i = x+1
+			j = y-1
+
+			while i<10 and j>-1 and obstacle == False :
+
+				if self.grid[i][j] == self.grid[x][y] :
+					obstacle = True
+
+
+				elif self.grid[i][j] > 0 :
+
+					if i+1<10 and j-1>-1 and takeover == False :
+						if self.grid[i+1][j-1] == 0 :
+							self.takeovers.append([i+1,j-1])
+							takeover = True
+							i = i+1
+							j = j-1
+						else :
+							obstacle = True
+					else :
+						obstacle = True
+
+
+				elif self.grid[i][j] == 0 :
+
+					if takeover == False :
+						self.moves.append([i,j])
+					else :
+						self.takeovers.append([i,j])
+
+				i = i+1
+				j = j-1
+
+
+
+			obstacle = False
+			takeover = False
+			i = x-1
+			j = y+1
+
+			while i>-1 and j<10 and obstacle == False :
+
+				if self.grid[i][j] == self.grid[x][y] :
+					obstacle = True
+
+
+				elif self.grid[i][j] > 0 :
+
+					if i-1>-1 and j+1<10 and takeover == False :
+						if self.grid[i-1][j+1] == 0 :
+							self.takeovers.append([i-1,j+1])
+							takeover = True
+							i = i-1
+							j = j+1
+						else :
+							obstacle = True
+					else :
+						obstacle = True
+
+
+				elif self.grid[i][j] == 0 :
+
+					if takeover == False :
+						self.moves.append([i,j])
+					else :
+						self.takeovers.append([i,j])
+
+				i = i-1
+				j = j+1
+
+
+
+			obstacle = False
+			takeover = False
+			i = x+1
+			j = y+1
+
+			while i<10 and j<10 and obstacle == False :
+
+				if self.grid[i][j] == self.grid[x][y] :
+					obstacle = True
+
+
+				elif self.grid[i][j] > 0 :
+
+					if i+1<10 and j+1<10 and takeover == False :
+						if self.grid[i+1][j+1] == 0 :
+							self.takeovers.append([i+1,j+1])
+							takeover = True
+							i = i+1
+							j = j+1
+						else :
+							obstacle = True
+					else :
+						obstacle = True
+
+
+				elif self.grid[i][j] == 0 :
+
+					if takeover == False :
+						self.moves.append([i,j])
+					else :
+						self.takeovers.append([i,j])
+
+				i = i+1
+				j = j+1
+
+
+	def check_end (self) :
+
+		i = 0
+		self.end = True
+		winner = 'player2'
+		while i < len(self.grid) and self.end == True :
+			if 1 in self.grid[i] :
+				self.end = False
+				winner = 'none'
+			i = i+1
+
+		if self.end == True :
+			return winner
+
+		i = 0
+		self.end = True
+		winner = 'player1'
+		while i < len(self.grid) and self.end == True :
+			if 2 in self.grid[i] :
+				self.end = False
+				winner = 'none'
+			i = i+1
+
+		if self.end == True :
+			return winner
+
+		else:
+			return ""
+
+
+
+	def partie_en_cours(self):
+
+		#send méthode: loop (afficher "à ton tour de jouer") JOUEUR C
+		#send méthode: wait, JOUEUR QUI ATTEND
+
+		#recv identifiant; x; y
+
+		self.left_click(x, y, identifiant_joueur)
+
+		#On vérifie si la partie est terminée
+		self.check_end()
+
+
+
+def main_serveur():
+	
+	#Creation de la partie
+	partie = Board(ip1,ip2)
+
+	#send à chaque joueur joueur1/joueur2
+	#send methode: dessiner la grille; arguments: grille
+
+	while not partie.end:
+
+		partie.partie_en_cours()
+		self.player = 3-self.player
+
+
+
