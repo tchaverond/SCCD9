3029a8ec0c7bb66fef89fcf11c49794d264e40e9
thomas.chaverondier@gmail.com <thomas.chaverondier@gmail.com>
2016-02-19 00:39:18 +0100

    Password entry field now only displays '*'

--- client.py
+++ client.py
@@ -24,7 +24,7 @@ class LoginWindow(tkDialog.Dialog):
 		Label(master, text="Password:").grid(row=1)
 
 	 	self.e1 = Entry(master)
-		self.e2 = Entry(master)
+		self.e2 = Entry(master, show="*")
 
 		self.e1.grid(row=0, column=1)
 		self.e2.grid(row=1, column=1)
